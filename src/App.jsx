7236848a2fc9e1508adc1a9fc14a11e0a95af61c
Venugopal Achhe <avenugopal999@gmail.com>
2025-10-10 22:49:47 +0200

    ADD: Adding product page to main site

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Academy from "./pages/Academy.jsx";
