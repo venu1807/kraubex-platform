d8e521e5ce0eae1935d9aa29c58be1dfe3b207a3
Venugopal Achhe <avenugopal999@gmail.com>
2025-10-10 23:23:56 +0200

    CHG:Changa vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/", // must be '/' for custom domains like www.kraubex.de
+  base: './', // Important for GitHub Pages
   plugins: [react()],
 });
